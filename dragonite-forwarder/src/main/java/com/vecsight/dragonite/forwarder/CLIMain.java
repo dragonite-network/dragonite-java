8c8ba9ad4f5a7d99ad391b776fa2af427f1f5864
Toby Huang <tobyxdd@gmail.com>
2017-06-12 13:57:38 +0800

    forwarder: fix openWebPanel on some platforms

--- dragonite-forwarder/src/main/java/com/vecsight/dragonite/forwarder/CLIMain.java
+++ dragonite-forwarder/src/main/java/com/vecsight/dragonite/forwarder/CLIMain.java
@@ -147,7 +147,7 @@ public class CLIMain {
             } else {
                 return false;
             }
-        } catch (URISyntaxException | IOException e) {
+        } catch (URISyntaxException | IOException | UnsupportedOperationException e) {
             return false;
         }
     }
