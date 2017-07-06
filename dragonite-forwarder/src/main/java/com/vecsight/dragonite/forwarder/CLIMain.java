fbf18f515b8f9691e7897d43a395358317c6c395
Toby Huang <tobyxdd@gmail.com>
2017-07-06 11:25:52 +0800

    add VectorSight confidential headers

--- dragonite-forwarder/src/main/java/com/vecsight/dragonite/forwarder/CLIMain.java
+++ dragonite-forwarder/src/main/java/com/vecsight/dragonite/forwarder/CLIMain.java
@@ -1,3 +1,16 @@
+/*
+ * VECTORSIGHT CONFIDENTIAL
+ * ------------------------
+ * Copyright (c) [2015] - [2017]
+ * VectorSight Systems Co., Ltd.
+ * All Rights Reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Toby Huang <t@vecsight.com>, June 2017
+ */
+
 package com.vecsight.dragonite.forwarder;
 
 import com.vecsight.dragonite.forwarder.config.ForwarderClientConfig;
